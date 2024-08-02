94a391a9d3a5459ecf13d8426a407f5fe40bd905
Brandon Garza <brandon@Brandons-MacBook-Air.local>
2024-08-02 07:34:14 -0500

    about_me

--- src/components/About.jsx
+++ src/components/About.jsx
@@ -11,14 +11,14 @@ const About = () => {
                 <div className='m-w-[1000px] w-full grid grrid-cols-2 gap-8'>
                 </div>
                 <div className='sm:text-right text-4xl font-bold text-gray-300'>
-                    <p>Hello, I'm Brandon nice to meet you. Please take a look around.</p>
+                    <p>Hello, I'm Brandon Garza.</p>
                 </div>
                 <div>
-                    <p className='text-gray-300'>I am passionate about building excellent software that improves 
-                       the loves of those around me. I specializ increating software for 
-                       clients ranging from indiiduals and small-businesses all the way 
-                       to large enterprise corporations. What would you do if you had a 
-                       software expert available at your fingertips. </p></div>
+                    <p className='text-gray-300'>I graduated from the University of Texas Rio Grande Valley in May 2023, and received a Bachelor's of Science in Computer Engineering.
+                    I also interned at the ECISD IT department during my last semester in school from December 2022 through November 1, 2023. Since then I have been working
+                    at a Commercial Real Estate company, researching the market. I am a gamer, I enjoy first-person shooters, sports, survival, rpg's, Call of Duty, PUBG, Rust, Grand Theft Auto,
+                    God of War, Sekiro, you name it. I like to be healthy, and trying out new restaurants, playing soccer, football, baseball, going out with friends, checking out new places, going on trips,
+                    being outdoors, fishing and hunting. I can go on and on.  </p></div>
             </div>
         </div>
     </div>
